b64f381884c67aa41684010c724c7fd6da0586ec
alberto <alberto@alberto-Ideapad-Z570>
2013-08-07 01:08:44 -0500

    Update v2

--- src/Algorithms/Practica1.py
+++ src/Algorithms/Practica1.py
@@ -5,44 +5,69 @@ Created on Aug 6, 2013
 '''
 
 class Practica1(object):
-    def __init__(self):#1 => Algoritmo Ingenuo
+    def __init__(self):  # 1 => Algoritmo Ingenuo
         self.cadena = [] 
-    def getArray(self,archivo='arreglo.csv'):
+    def recursiveSolution(self, archivo='arreglo.csv'):
         if(self.__readFile(archivo) is True):
             self.cadena = self.archivo.split(",")
-            self.findPeak(len(self.cadena)/2,self.cadena)
+            self.findPeak(len(self.cadena) / 2, self.cadena)
         else:
             print "ERROR AL LEER EL ARCHIVO"
         
-    def findPeak(self,middle,cadena):
-        print "Cadena ",cadena," \n Mitad ",cadena[middle]
+    def findPeak(self, middle, cadena):
+        print "Cadena", cadena, "\nMitad", cadena[middle], "\nIndice", middle
         try:
-            if(cadena[middle>=cadena[middle+1]] and cadena[middle]>=cadena[middle-1]):
-                print "\n Pico encontrado en ",cadena[middle]
+            if(cadena[middle >= cadena[middle + 1]] and cadena[middle] >= cadena[middle - 1]):
+                print "\nPico encontrado en", cadena[middle]
                 return
             else:
-                if(cadena[middle+1]>=cadena[middle]):#Se compara el lado derecho
-                    print cadena[middle+1],">=",cadena[middle]
-                    cadenaAux = cadena[middle+1:]
-                    self.findPeak(len(cadenaAux)/2, cadenaAux)
+                if(cadena[middle + 1] >= cadena[middle]):  # Se compara el lado derecho
+                    print cadena[middle + 1], ">=", cadena[middle]
+                    cadenaAux = cadena[middle + 1:]
+                    self.findPeak(len(cadenaAux) / 2, cadenaAux)
                     return
                 else:
-                    print cadena[middle+1],"<=",cadena[middle]
-                    cadenaAux = cadena[:middle-1]
-                    self.findPeak(len(cadenaAux)/2, cadenaAux)
+                    print cadena[middle + 1], "<=", cadena[middle]
+                    cadenaAux = cadena[:middle - 1]
+                    self.findPeak(len(cadenaAux) / 2, cadenaAux)
                     return
         except Exception as exception:
-            print "\n Array Index Out of Bounds Exception ",exception
-                    
-    def __readFile(self,fileName):
+            print "\nArray Index Out of Bounds Exception", exception
+    def iterativeSolution(self, archivo='arreglo.csv'):
+        pass                    
+    def __readFile(self, fileName):
         try:
-            self.archivo = open(fileName,'r').read()
+            self.archivo = open(fileName, 'r').read()
             return True
         except Exception as exception:
             print exception
             return False
         
 start = Practica1()
-start.getArray()
-#start.findPeak(2)
+archivo = "" 
+try:
+    print "Selecciona una opcion para ejecutar "
+    print "\t 1) Algoritmo Ingenuo"
+    print "\t 2) Algoritmo Recursivo"
+    mode = int(raw_input('Opcion:'))
+    while(not(isinstance(mode, int))):
+        print "Selecciona una opcion valida :"
+        mode = int(raw_input('Opcion:'))
+    archivo = str(raw_input("Nombre del archivo (vacio para archivo por default): "))            
+    if(mode is 1):
+        # Algoritmo ingenuo
+        if(len(archivo) > 0):
+            start.iterativeSolution(archivo)
+        else:
+            start.iterativeSolution()
+        pass
+    if(mode is 2):
+        # Algoritmo Recursivo
+        if(len(archivo) > 0):
+            start.recursiveSolution(archivo)
+        else:
+            start.recursiveSolution()
+        pass
+except Exception as exception:
+    print "Exception ", exception
         
